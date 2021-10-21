fa4aa2b5ae7b795677d87b1470f17abe7e9a4e58
daniel-namir <ddnnll@email.arizona.edu>
2021-10-20 21:27:10 -0700

    update

--- index.js
+++ index.js
@@ -70,7 +70,8 @@ application.post('/score', (request, response) => {
 application.get('/scores/:quiztaker/:quizid', (request, response) => {
     let quizTaker = request.params.quiztaker;
     let quizId = request.params.quizid;
-    response.json(api.checkScore(quizTaker, quizId));
+    let quizScore = api.checkScore(quizTaker, quizId);
+    response.send(JSON. stringify(quizScore));
 });
 
 application.listen(port, () => console.log('Listening on port ' + port));
