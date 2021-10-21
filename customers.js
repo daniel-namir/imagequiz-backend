189fda403341fde3e21a2e1e03ae0e7ccd791648
daniel-namir <ddnnll@email.arizona.edu>
2021-10-20 21:44:57 -0700

    update

--- customers.js
+++ customers.js
@@ -2,6 +2,6 @@ let customers = [];
 
 //When you add a new record to the customer, you should use the following signature for the 
 //object that you are adding to the list (id should be a unique number):
-customers.push({id: 0, name: '', password: ''});
+//customers.push({id: 0, name: '', password: ''});
 
 exports.customers = customers; 
--- scores.js
+++ scores.js
@@ -2,6 +2,6 @@ let scores = [];
 
 //When you add a new record to the score list follow the following signature 
 //(quizTaker should be the email of the user):
-scores.push({quizTaker: '', quizId: '', score: '', date: ''});
+//scores.push({quizTaker: '', quizId: '', score: '', date: ''});
 
 exports.scores = scores;
